68848f483022098cb7db0ea755b08a6fb91b4c9f
Adinoyi Uzal Sule <ussyboy62@gmail.com>
2026-08-15 02:47:05 +0100

    Add files via upload

---
+++ project_10_simple_login.py
@@ -0,0 +1,25 @@
+username = "admin"
+password = "1234"
+
+print("===== SIMPLE LOGIN SYSTEM =====")
+
+user = input("Enter username: ")
+user_password = input("Enter password: ")
+
+if user == username and user_password == password:
+    print("Login successful!")
+    print("Welcome,", user)
+else:
+    print("Invalid username or password.")username = "admin"
+password = "1234"
+
+print("===== SIMPLE LOGIN SYSTEM =====")
+
+user = input("Enter username: ")
+user_password = input("Enter password: ")
+
+if user == username and user_password == password:
+    print("Login successful!")
+    print("Welcome,", user)
+else:
+    print("Invalid username or password.")
