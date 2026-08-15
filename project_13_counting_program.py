831a540551b9b729d12e376871d8a6598fd45518
Adinoyi Uzal Sule <ussyboy62@gmail.com>
2026-08-15 03:10:18 +0100

    Add files via upload

---
+++ project_13_counting_program.py
@@ -0,0 +1,7 @@
+print("===== COUNTING PROGRAM =====")
+
+start = int(input("Enter the starting number: "))
+end = int(input("Enter the ending number: "))
+
+for number in range(start, end + 1):
+    print(number)
